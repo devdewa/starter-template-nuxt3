1b9bf83cf9d0918347ce70235b2ec1f84fea903f
devdewa <uchiasasuke123.sp@gmail.com>
2023-03-21 18:33:36 +0700

    Release

---
+++ app.vue
@@ -0,0 +1,17 @@
+<template>
+
+  <!-- Navigation -->
+  <nav class="bg-slate-900 py-6 flex justify-center">
+    <ul class="flex gap-20"> 
+      <li class="text-white"><NuxtLink :to="{ name: 'index' }">Home</NuxtLink></li>
+      <li class="text-white"><NuxtLink :to="{ name: 'about' }">About</NuxtLink></li>
+      <li class="text-white"><NuxtLink :to="{ name: 'store' }">Store</NuxtLink></li>
+      <li class="text-white"><NuxtLink :to="{ name: 'guide' }">Guide</NuxtLink></li>
+    </ul>
+  </nav>
+
+  <!-- Content Section -->
+  <section class="min-h-screen bg-slate-800 text-white">
+    <NuxtPage />
+  </section>
+</template>
